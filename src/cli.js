e06b61fb3bdf67e5bce0543dcb3fc71a6e97c88b
ArtZhevngit config --global user.email kingofcornflakes132@yandex.ru <kingofcornflakes132@yandex.ru>
2022-09-25 04:38:02 +0300

    made export of greetings

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export let exportedName = '';
+export const greetings = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  exportedName = name;
 };
