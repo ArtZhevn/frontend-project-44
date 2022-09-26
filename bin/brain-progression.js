7c53f0d7c64938fb3d795d4b0e5a5652c2a2492f
ArtZhevngit config --global user.email kingofcornflakes132@yandex.ru <kingofcornflakes132@yandex.ru>
2022-09-26 19:43:43 +0300

    made game of progression

---
+++ bin/brain-progression.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+export default () => {
+  console.log('brain-calc');
+  console.log('');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('What number is missing in the progression?');
+  let i = 0;
+  while (i < 3) {
+    let beginOfArray = Math.floor(Math.random() * 20) + 1;
+    const stepOfProgression = Math.floor(Math.random() * 9) + 2;
+    const emptyElement = Math.floor(Math.random() * 10) + 1;
+    const arrOfProgression = [];
+    while (arrOfProgression.length < 10) {
+      arrOfProgression.push(beginOfArray);
+      beginOfArray += stepOfProgression;
+    }
+    const answer = arrOfProgression[emptyElement];
+    arrOfProgression[emptyElement] = '..';
+    const question = readlineSync.question(`Question: ${arrOfProgression} `);
+    console.log(`Your answer: ${question}`);
+    if (Number(question) === answer) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`${question} is wrong answer ;(. Correct answer was ${answer}.`);
+      break;
+    }
+  }
+  if (i === 3) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}`);
+  }
+};
