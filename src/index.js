476c4585eef5e111a9f922416c9c139fc6a85474
ArtZhevngit config --global user.email kingofcornflakes132@yandex.ru <kingofcornflakes132@yandex.ru>
2022-09-28 06:40:22 +0300

    made file for consts and other functions

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-mutable-exports */
+import readlineSync from 'readline-sync';
+
+export let exportedName;
+export const greetings = () => {
+  console.log('');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  exportedName = name;
+};
+
+export const rightAnswer = () => {
+  console.log(`Congratulations, ${exportedName}!`);
+};
+
+export const wrongAnswer = () => {
+  console.log(`Let's try again, ${exportedName}!`);
+};
+
+export const lastRandomNumber = 19;
+export const firstRandomNumber = 1;
+export const positiveResponse = 'yes';
+export const negativeResponse = 'no';
