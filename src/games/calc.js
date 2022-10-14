c47ea6c5543a1d18e25933c43182a0124d25ba1c
ArtZhevngit config --global user.email kingofcornflakes132@yandex.ru <kingofcornflakes132@yandex.ru>
2022-10-14 13:19:57 +0300

    fix random

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,11 +4,13 @@ import startCalc from '../index.js';
 
 const description = 'What is the result of the expression?';
 const lastRandomIndex = 3;
+const minRandomNumber = 1;
+const maxRandomNumber = 19;
 let expression;
 const logicOfTheGame = () => {
-  const number1 = getRandomNumber();
-  const number2 = getRandomNumber();
-  const indexOfMathOperation = getRandomNumber(lastRandomIndex);
+  const number1 = getRandomNumber(minRandomNumber, maxRandomNumber);
+  const number2 = getRandomNumber(minRandomNumber, maxRandomNumber);
+  const indexOfMathOperation = getRandomNumber(minRandomNumber, lastRandomIndex);
   let answer;
   if (indexOfMathOperation === 1) {
     expression = `${number1} + ${number2}`;
