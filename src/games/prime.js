7a5c992d60f1e5bc60dc103d146a8b0e0c1c570e
ArtZhevngit config --global user.email kingofcornflakes132@yandex.ru <kingofcornflakes132@yandex.ru>
2022-10-18 02:47:44 +0300

    make isPrime function

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,26 +2,27 @@ import startPrime from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (checkNumber) => {
+  if (checkNumber < 2) {
+    return false;
+  }
+  for (let i = 2; i <= checkNumber / 2; i += 1) {
+    if (checkNumber % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const logicOfTheGame = () => {
   let answer;
-  const allDeviders = [];
-  const arrOfDeviders = [];
-  const isPrime = getRandomNumber(1, 19);
-  for (let firstElementOfArr = 1; firstElementOfArr <= isPrime; firstElementOfArr += 1) {
-    arrOfDeviders.push(firstElementOfArr);
-  }
-  for (let indexOfDeviders = 0; indexOfDeviders < arrOfDeviders.length; indexOfDeviders += 1) {
-    if (isPrime % arrOfDeviders[indexOfDeviders] === 0) {
-      allDeviders.push(arrOfDeviders[indexOfDeviders]);
-    }
-  }
-  if (allDeviders.length <= 2 && isPrime !== 1) {
+  const checkNumber = getRandomNumber(1, 19);
+  if (isPrime(checkNumber) === true) {
     answer = 'yes';
   } else {
     answer = 'no';
   }
-  return [isPrime, answer];
+  return [checkNumber, answer];
 };
 
 const runGame = () => {
