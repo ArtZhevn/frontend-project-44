31656c5609a716aaf690e1282995006ab9c152cf
ArtZhevngit config --global user.email kingofcornflakes132@yandex.ru <kingofcornflakes132@yandex.ru>
2022-10-20 00:46:18 +0300

    remove else from index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import readlineSync from 'readline-sync';
 
+const roundsCount = 3;
+
 export default (description, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
-  const gameRounds = 3;
-  for (let count = 0; count < gameRounds; count += 1) {
+  for (let count = 0; count < roundsCount; count += 1) {
     const [expression, correctAnswer] = generateRound();
     const answer = readlineSync.question(`Question: ${expression} `);
     console.log(`Your answer: ${answer}`);
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (answer !== correctAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
-    }
+    } console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
